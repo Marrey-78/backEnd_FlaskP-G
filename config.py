b96191003d8068a9c3dde337cf4445bd387dc499
Marrey-78 <gabrieleT98@gmail.com>
2025-07-07 12:34:21 +0200

    init flask

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+DB_HOST = "caboose.proxy.rlwy.net"
+DB_USER = "root"
+DB_PASSWORD = "jAFIiYFsyZFvSisaJPzCMmvHnLqqJyYJ"
+DB_NAME = "railway"
+DB_PORT = 17978
+
---
+++ flask_api.py
@@ -0,0 +1,45 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from config import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT
+import mysql.connector
+
+app = Flask(__name__)
+CORS(app)
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host= DB_HOST,
+        user= DB_USER,
+        password= DB_PASSWORD,
+        database= DB_NAME,
+        port= DB_PORT
+    )
+
+@app.route('/trips', methods=['GET'])
+def get_trips():
+    continent = request.args.get('continent')  # filtro opzionale
+    month = request.args.get('month')  # filtro opzionale
+
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    #query = "SELECT d.id, d.name, d.country, c.name AS continent, b.name AS brand FROM destinations d JOIN continents c ON d.continent_id = c.id JOIN brand b ON d.brand_id = b.id"
+    #query = f"SELECT t.price, t.month, t.start_date, t.end_date, d.name AS destination_name, d.country AS country_name, c.name AS continent_name, b.name AS brand_name FROM trips t JOIN destinations d ON t.destination_id = d.id JOIN continents c ON d.continent_id = c.id JOIN brand b ON d.brand_id = b.id;";
+    query = f"SELECT t.price, t.month, t.start_date, t.end_date, t.age, t.link, t.nights, d.name AS destination_name, d.country AS country_name, c.name AS continent_name, b.name AS brand_name FROM trips t JOIN destinations d ON t.destination_id = d.id JOIN continents c ON d.continent_id = c.id JOIN brand b ON d.brand_id = b.id WHERE t.month = '{month}' AND c.name = '{continent}';";
+    #query = f"SELECT t.* FROM trips t JOIN destinations d ON t.destination_id = d.id JOIN continents c ON d.continent_id = c.id JOIN brand b ON d.brand_id = b.id WHERE t.month = {month} AND c.name = {continent};";
+    #params = []
+
+    #if continent:
+        #query += " WHERE c.name = %s"
+        #params.append(continent)
+
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return jsonify(results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
